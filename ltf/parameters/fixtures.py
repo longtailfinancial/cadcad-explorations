dad96d4aea3746aff51fc4ff3ff26c312798f628
Shawn Anderson <shawnltf@pm.me>
2022-03-24 00:13:29 -0700

    Major updates.

---
+++ ltf/parameters/fixtures.py
@@ -0,0 +1,166 @@
+distributions = [
+    {
+        'founders': 0.1,
+        'dao_rewards': 0.5,
+        'lbp_pool': 0.4,
+    },
+    {
+        'founders': 0.3,
+        'dao_rewards': 0.5,
+        'lbp_pool': 0.2,
+    },
+]
+
+
+roles = [
+    {
+        'name': 'CEO',
+        'base_wage': 2000,
+        'description': 'Highest level decision maker.'
+    },
+    {
+        'name': 'CFO',
+        'base_wage': 2000,
+        'description': 'Bottoms-lines guy.'
+    },
+    {
+        'name': 'CTO',
+        'base_wage': 2000,
+    },
+    {
+        'name': 'Admin',
+        'base_wage': 1600,
+    },
+    {
+        'name': 'CMO',
+        'base_wage': 2000,
+    },
+    {
+        'name': 'Token Engineer 1',
+        'base_wage': 2000,
+    },
+    {
+        'name': 'Token Engineer 2',
+        'base_wage': 3000,
+    },
+    {
+        'name': 'Racoon',
+        'base_wage': 3000,
+    }
+]
+
+bootcamp_roles = [
+    {
+        'name': 'Data Wizard',
+        'base_wage': 2000,
+        'description': 'Highest level decision maker.'
+    },
+    {
+        'name': 'Community Steward',
+        'base_wage': 2000,
+        'description': 'Bottoms-lines guy.'
+    },
+    {
+        'name': 'Developer',
+        'base_wage': 2000,
+    },
+    {
+        'name': 'Writer',
+        'base_wage': 2000,
+        'description': 'Bottoms-lines guy.'
+    },
+]
+
+import datetime as dt
+founders = [
+    {
+        'authorization_level' : 5,
+        'conviction_level': 5,
+        'credentials_level': 4,
+        'date_joined': dt.date(2018, 8, 18),
+        'engagement': 'Full Time',
+        'first_name': 'Shawn',
+        'last_name': 'Anderson',
+        'role_name': 'CEO',
+    },
+    {
+        'authorization_level' : 3,
+        'conviction_level': 4,
+        'credentials_level': 1,
+        'date_joined': dt.date(2019, 8, 18),
+        'engagement': 'Part Time',
+        'first_name': 'Aidan',
+        'last_name': 'Oconnel',
+        'role_name': 'Admin',
+    },
+    {
+        'authorization_level' : 4,
+        'conviction_level': 5,
+        'credentials_level': 3,
+        'date_joined': dt.date(2021, 4, 15),
+        'engagement': 'Full Time',
+        'first_name': 'Mike',
+        'last_name': 'De Melo',
+        'role_name': 'CFO',
+    },
+    {
+        'authorization_level' : 4,
+        'conviction_level': 5,
+        'credentials_level': 3,
+        'date_joined': dt.date(2021, 5, 15),
+        'engagement': 'Full Time',
+        'first_name': 'Jake',
+        'last_name': 'Cassani',
+        'role_name': 'CTO',
+    },
+    {
+        'first_name': 'Liam',
+        'last_name': 'Perison',
+        'authorization_level' : 4,
+        'conviction_level': 4,
+        'credentials_level': 1,
+        'date_joined': dt.date(2021, 5, 15),
+        'engagement': 'Full Time',
+        'role_name': 'Admin',
+    },
+    {
+        'first_name': 'BK',
+        'last_name': 'K',
+        'authorization_level' : 3,
+        'conviction_level': 4,
+        'credentials_level': 3,
+        'date_joined': dt.date(2021, 5, 15),
+        'engagement': 'Full Time',
+        'role_name': 'Token Engineer 1',
+    },
+    {
+        'first_name': 'Katie',
+        'last_name': 'Knorr',
+        'authorization_level' : 4,
+        'conviction_level': 3,
+        'credentials_level': 3,
+        'date_joined': dt.date(2021, 8, 15),
+        'engagement': 'Part Time',
+        'role_name': 'CMO',
+    },
+    {
+        'first_name': 'Andrew',
+        'last_name': 'Penland',
+        'authorization_level' : 3,
+        'conviction_level': 5,
+        'credentials_level': 5,
+        'date_joined': dt.date(2021, 8, 15),
+        'engagement': 'Part Time',
+        'role_name': 'Racoon',
+    },
+    {
+        'first_name': 'Zhiwei',
+        'last_name': 'Li',
+        'authorization_level' : 2,
+        'conviction_level': 2,
+        'credentials_level': 5,
+        'date_joined': dt.date(2022, 3, 6),
+        'engagement': 'Full Time',
+        'role_name': 'Token Engineer 1',
+    }
+]
---
+++ octopus/simpy.py
@@ -0,0 +1,10 @@
+import sympy
+from sympy import symbols
+
+a_1, a_1prime1 = symbols('a_1'), symbols('a_1prime1')
+
+p1,p2,p3,p4 = [symbols(x) for x in ["p1", "p2", "q1", "q2"]]
+
+v_prime1 = symbols("vprime_1")
+
+f = Function('f')
